0f2284634b25a7313ad5c7edb2a9885436613728
Harry-Lin1123 <lin11232814@gmail.com>
2024-05-30 15:20:12 +0800

    先把部落格

--- index/src/app/app.component.ts
+++ index/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink } from '@angular/router';
+import { PostComponent } from './post/post.component';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PostComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
