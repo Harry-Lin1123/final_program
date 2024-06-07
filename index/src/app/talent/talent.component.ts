a6c515c8787f311a6f7c6c4ddcf6bf979a536950
Harry-Lin1123 <lin11232814@gmail.com>
2024-06-08 05:06:36 +0800

    有些手機

--- index/src/app/talent/talent.component.ts
+++ index/src/app/talent/talent.component.ts
@@ -15,4 +15,5 @@ export class TalentComponent {
     {img:'./assets/img/top_talents_hololive_en.png', img_logo:'./assets/img/top_logo_hololive_en.svg'},
     {img:'./assets/img/top_talents_devis.png', img_logo:'./assets/img/top_logo_devis.svg'},
   ]
+  text='hololive production TALENT'
 }
