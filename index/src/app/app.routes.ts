84178ec38b18a2e2e26afa763a8c3622a4402d70
b11005007 <b11005007@ctbc.edu.tw>
2024-06-08 03:07:12 +0800

    網址

--- index/src/app/app.routes.ts
+++ index/src/app/app.routes.ts
@@ -1,4 +1,3 @@
-import { WebNewsComponent } from './web-news/web-news.component';
 import { Routes } from '@angular/router';
 import { PostComponent } from './post/post.component';
 import { HomeComponent } from './home/home.component';
@@ -7,12 +6,16 @@ import path from 'path';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { WebAboutComponent } from './web-about/web-about.component';
+import { WebTalentComponent } from './web-talent/web-talent.component';
+import { WebNewsComponent } from './web-news/web-news.component';
 
 
 export const routes: Routes = [
  {path: '', component: HomeComponent },
  {path:'post/:id', component: PostComponent},
- {path:'app-web-about', component: WebAboutComponent}
+ {path:'about', component: WebAboutComponent},
+ {path:'talent', component: WebTalentComponent},
+ {path:'news', component: WebNewsComponent}
 
  //{path:'', component: AppComponent}
 
