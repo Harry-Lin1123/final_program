4cd307dc398303aa54432692d6b0ca536b930ee6
Harry-Lin1123 <lin11232814@gmail.com>
2024-06-09 03:01:04 +0800

    家玩footer

--- index/src/app/app.component.ts
+++ index/src/app/app.component.ts
@@ -7,10 +7,22 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,PostComponent],
+  imports: [RouterOutlet,PostComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'index';
+  twitter_link='https://twitter.com/intent/tweet?text=ホロライブプロダクション / @hololivetv より &amp;url=https://hololivepro.com/&amp;hashtags=%E3%83%9B%E3%83%AD%E3%83%A9%E3%82%A4%E3%83%96%E3%83%97%E3%83%AD%E3%83%80%E3%82%AF%E3%82%B7%E3%83%A7%E3%83%B3';
+  twitter_img='https://hololivepro.com/wp-content/themes/hololive_production/images/common_icon_x_jp.svg';
+
+  icons=[{link:'https://social-plugins.line.me/lineit/share?url=https://hololivepro.com',
+    img:'https://hololivepro.com/wp-content/themes/hololive_production/images/common_icon_line.svg'
+  },{link:'https://www.reddit.com/submit?url=https://hololivepro.com',
+    img:'https://hololivepro.com/wp-content/themes/hololive_production/images/common_icon_reddit.svg'
+  },{link:'http://www.facebook.com/share.php?u=https://hololivepro.com',
+    img:'https://hololivepro.com/wp-content/themes/hololive_production/images/common_icon_facebook.svg'
+  }]
+
+
 }
